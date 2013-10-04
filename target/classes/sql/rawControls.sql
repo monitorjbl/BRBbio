15f73da183111b06ce663aea231787cc0e012f9d
Taylor Jones <monitorjbl@gmail.com>
2013-10-03 22:17:07 -0400

    adding code for user authentication

--- target/classes/sql/rawControls.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT c.identifier FROM plates p 
-	JOIN raw_data_controls c ON c.plate_id = p.id 
-	JOIN user_security rs ON rs.run_id = p.run_id
-WHERE p.run_id=? AND rs.user_name = ?
--- target/classes/sql/run.sql
+++
@@ -1,3 +0,0 @@
-SELECT r.* FROM runs r 
-	JOIN user_security rs ON rs.run_id = r.id
-WHERE rs.user_name = ?
